831a402871ed1ac8a61dfc4079365debc5e545eb
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-11-26 17:49:01 +0530

    Added Logic in Dashboard to connect to metamask component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,6 +6,17 @@ import "./dashboard.css";
 import ConnectMetamask from "../Utils/ConnectMetamask/ConnectMetamask";
 
 export default function Dashboard(props) {
+	function isOwner(_song) {
+		return _song.currentOwnerAddress === props.account;
+	}
+
+	function isCreator(_song) {
+		return _song.artistAddress === props.account;
+	}
+
+	useEffect(() => {
+		ConnectMetamask("connectButton", "Install MetaMask", "Connect with Metamask");
+	}, []);
 
 	return (
 		<Fragment>
