df67dc0a6965d71aa88889fb2fabe60f04b703d1
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-11-29 10:12:55 +0530

    Added Trending Card Component

---
+++ src/components/Trending/Trending.js
@@ -0,0 +1,55 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import "./trending.css";
+import TrendingCard from "./TrendingCard";
+import trendingIcon from "../../assets/trendingIcon.svg";
+
+export default function Trending(props) {
+
+	return (
+		<Fragment>
+			<div className="trending_container">
+				<div className="trending_box">
+					<div className="row justify-content-center">
+						{/* Trending Cards Row */}
+						<div className="trending_bg_glass">
+							<p className="trending_heading">Trending</p>
+							<div className="trending_cards_div">
+								<div className="row trending_cards_row justify-content-center m-0 p-0">
+									{props.songNFTs.length > 0 ? (
+										trendingCards
+									) : (
+										<div className="row justify-content-center text-center mt-3 mb-5">
+											<h2>No songs to display</h2>
+										</div>
+									)}
+								</div>
+							</div>
+						</div>
+
+						{/* CTA Create Trends */}
+						<div className="mt-5 cta_create_trend row justify-content-center">
+							<img src={trendingIcon} alt="create trends" />
+							<div className="row justify-content-center">
+								<div className="cta_trend_text">
+									<p>Don't follow trends. Start trends!</p>
+									<span>Upload your songs on MXV</span>
+								</div>
+							</div>
+
+							<Link to={"/create"}>
+								<div className="mt-4 row justify-content-center create_trend_btn">
+									<button className="btn shadow-sm col-md-6 col-sm-8 col-8 send_message_btn">
+										<span>
+											Start Creating<i className="fas fa-angle-double-right"></i>
+										</span>
+									</button>
+								</div>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
+}
