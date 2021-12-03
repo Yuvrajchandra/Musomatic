4c39ec40adeda94bc325c97398ea4c2751799083
Pushpit <pushpit07@gmail.com>
2021-12-03 20:54:09 +0530

    Created functions for backend connection
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 // Importing packages
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import Web3 from "web3";
+import axios from "axios";
 // Importing abi
 import Musomatic from "../abis/Musomatic.json";
 // Importing css
@@ -12,20 +13,399 @@ import Footer from "./Layout/Footer/Footer";
 import ScrollToTop from "./Utils/ScrollToTop/ScrollToTop";
 import Loading from "./Utils/Loading/Loading";
 import HomePage from "./Homepage/HomePage";
+import Library from "./Library/Library";
+import Dashboard from "./Dashboard/Dashboard";
+import Trending from "./Trending/Trending";
 import Create from "./Create/Create";
 import PageNotFound from "./PageNotFound/PageNotFound";
-import Library from "./Library/Library";
+// Importing ENV variables
+import contractAddress from "../config/address";
+import configData from "../config/config.json";
+const ENV = JSON.parse(JSON.stringify(configData));
+
+// Initializing IPFS and StreamChat clients
+const ipfsClient = require("ipfs-http-client");
 
 function App() {
+	var enc = new TextEncoder();
+	// Initializing states
+	const [loading, setLoading] = useState(true);
+	const [account, setAccount] = useState("");
+	const [musomatic, setMusomatic] = useState("");
+	const [songNFTs, setSongNFTs] = useState([]);
+	const [imageBuffer, setImageBuffer] = useState(Buffer(enc.encode("-")));
+	const [songBuffer, setSongBuffer] = useState(Buffer(enc.encode("-")));
+	const [songLyrics, setSongLyrics] = useState("");
+	const [lyricsBuffer, setLyricsBuffer] = useState(Buffer(enc.encode("-")));
+	const [songDescription, setSongDescription] = useState("");
+	const [descriptionBuffer, setDescriptionBuffer] = useState(Buffer(enc.encode("-")));
+	const [maticUSD, setMaticUSD] = useState("");
+	const [maticINR, setMaticINR] = useState("");
+	var web3 = useRef();
+
+	const ipfs = ipfsClient({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
+
+	const { ethereum } = window;
+	if (ethereum) {
+		ethereum.on("accountsChanged", async function (accounts) {
+			if (web3.current) {
+				setAccount(web3.current.utils.toChecksumAddress(accounts[0]));
+			}
+			window.location.reload();
+		});
+	}
+
+	async function fetchMaticUSD() {
+		const COINBASE_BASE_URL = "https://api.coinbase.com/v2";
+		const res = await fetch(`${COINBASE_BASE_URL}/prices/MATIC-USD/buy`);
+		const data = await res.json();
+		setMaticUSD(data.data.amount);
+	}
+
+	async function fetchMaticINR() {
+		const COINBASE_BASE_URL = "https://api.coinbase.com/v2";
+		const res = await fetch(`${COINBASE_BASE_URL}/prices/MATIC-INR/buy`);
+		const data = await res.json();
+		setMaticINR(data.data.amount);
+	}
+
+	const loadBlockchainData = useCallback(async () => {
+		var accounts;
+
+		async function addPolygonTestnetNetwork() {
+			try {
+				await ethereum.request({
+					method: "wallet_switchEthereumChain",
+					params: [{ chainId: "0x13881" }], // Hexadecimal version of 80001, prefixed with 0x
+				});
+			} catch (error) {
+				if (error.code === 4902) {
+					try {
+						await ethereum.request({
+							method: "wallet_addEthereumChain",
+							params: [
+								{
+									chainId: "0x13881", // Hexadecimal version of 80001, prefixed with 0x
+									chainName: "POLYGON Testnet",
+									nativeCurrency: {
+										name: "MATIC",
+										symbol: "MATIC",
+										decimals: 18,
+									},
+									rpcUrls: ["https://matic-mumbai.chainstacklabs.com/"],
+									blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+									iconUrls: [""],
+								},
+							],
+						});
+					} catch (addError) {
+						console.log("Did not add network");
+					}
+				}
+			}
+		}
+
+		if (ethereum) {
+			window.web3 = new Web3(ethereum);
+			await addPolygonTestnetNetwork();
+		} else if (ethereum && (await ethereum.request({ method: "net_version" })) !== "80001") {
+			window.web3 = new Web3(window.web3.currentProvider);
+			await addPolygonTestnetNetwork();
+		} else {
+			// const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+			window.web3 = new Web3(new Web3.providers.HttpProvider("https://polygon-mumbai.infura.io/v3/6f89b4b5242a4191af04c7939d66d6e8"));
+			window.alert(
+				"Non-Ethereum browser detected. You cannot perform any transactions on the blockchain, however you will still be able to watch all content present on the blockchain. To make transactions you should consider installing Metamask"
+			);
+		}
+
+		web3.current = window.web3;
+
+		// This results in a bug which converts all uppercase alphabets in the address to lowercase
+		// accounts = await window.ethereum.request({ method: "eth_accounts" });
+		accounts = await web3.current.eth.getAccounts();
+		// accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+		// Load account
+		setAccount(accounts[0]);
+
+		const networkData = Musomatic.networks[ENV.BLOCKCHAIN_NETWORK_ID];
+		if (networkData) {
+			const _musomatic = new web3.current.eth.Contract(Musomatic.abi, networkData.address);
+			setMusomatic(_musomatic);
+
+			// Fetch all NFTs
+			const _nfts = await _musomatic.methods.fetchAllNFTs().call();
+			setSongNFTs(_nfts);
+
+			setLoading(false);
+		} else {
+			window.alert("Musomatic contract not deployed to detected network.");
+		}
+	}, [ethereum]);
+
+	useEffect(() => {
+		async function tasks() {
+			await loadBlockchainData();
+		}
+		tasks();
+		fetchMaticUSD();
+		fetchMaticINR();
+		// setLoading(false);
+	}, [loadBlockchainData, account]);
+
+	async function captureLyrics(event) {
+		event.preventDefault();
+
+		const file = new Blob([event.target.value.length > 0 ? event.target.value : "-"], { type: "text/plain" });
+
+		if (file) {
+			const reader = new window.FileReader();
+			reader.readAsArrayBuffer(file);
+
+			reader.onloadend = () => {
+				setLyricsBuffer(Buffer(reader.result));
+			};
+		} else {
+			return;
+		}
+	}
+
+	async function captureDescription(event) {
+		event.preventDefault();
+
+		const file = new Blob([event.target.value.length > 0 ? event.target.value : "-"], { type: "text/plain" });
+
+		if (file) {
+			const reader = new window.FileReader();
+			reader.readAsArrayBuffer(file);
+
+			reader.onloadend = () => {
+				setDescriptionBuffer(Buffer(reader.result));
+			};
+		} else {
+			return;
+		}
+	}
+
+	async function captureImage(event) {
+		event.preventDefault();
+
+		const file = event.target.files[0];
+		if (file) {
+			const reader = new window.FileReader();
+			reader.readAsArrayBuffer(file);
+
+			reader.onloadend = () => {
+				setImageBuffer(Buffer(reader.result));
+			};
+		} else {
+			return;
+		}
+	}
+
+	async function captureSong(event) {
+		event.preventDefault();
+
+		if (document.getElementById("upload-song")) {
+			var uploadSong = document.getElementById("upload-song");
+			if (uploadSong.files[0].size > 100700000) {
+				alert("File size is too big! Please upload a file smaller than 100MB.");
+				return;
+			}
+		}
+
+		const file = event.target.files[0];
+
+		if (file) {
+			const reader = new window.FileReader();
+			reader.readAsArrayBuffer(file);
+
+			reader.onloadend = () => {
+				setSongBuffer(Buffer(reader.result));
+			};
+		} else {
+			return;
+		}
+	}
+
+	async function createSong(_name, _artistName, _price, _onSale, _links, _characteristics) {
+		var _imgHash;
+		var _songHash;
+		var _lyricsHash;
+		var _descriptionHash;
+
+		setLoading(true);
+		await ipfs.add(imageBuffer, async (error, _imgResult) => {
+			console.log("_imgResult:", _imgResult);
+			if (error) {
+				console.error(error);
+				return;
+			}
+			_imgHash = _imgResult[0].hash;
+			console.log("imgHash:", _imgHash);
+
+			ipfs.add(songBuffer, async (error, _songResult) => {
+				console.log("_songResult:", _songResult);
+				if (error) {
+					console.error(error);
+					return;
+				}
+				_songHash = _songResult[0].hash;
+				console.log("_songHash:", _songHash);
+
+				ipfs.add(descriptionBuffer, async (error, _descriptionResult) => {
+					console.log("_descriptionResult:", _descriptionResult);
+					if (error) {
+						console.error(error);
+						return;
+					}
+					_descriptionHash = descriptionBuffer.equals(Buffer(enc.encode("-"))) ? [] : _descriptionResult[0].hash;
+					console.log("_descriptionHash:", _descriptionHash);
+
+					ipfs.add(lyricsBuffer, async (error, _lyricsResult) => {
+						console.log("_lyricsResult:", _lyricsResult);
+						if (error) {
+							console.error(error);
+							return;
+						}
+						_lyricsHash = lyricsBuffer.equals(Buffer(enc.encode("-"))) ? [] : _lyricsResult[0].hash;
+						console.log("_lyricsHash:", _lyricsHash);
+
+						// musomatic.methods
+						// 	.createSong(_name, _artistName, window.web3.utils.toWei(_price, "Ether"), _imgHash, _songHash, _descriptionHash, _lyricsHash, _onSale, _links, _characteristics)
+						// 	.send({ from: account })
+						// 	.on("transactionHash", () => {
+						// 		setLoading(false);
+						// 		window.location.reload();
+						// 	});
+
+						// Set up your Ethereum transaction
+						const transactionParameters = {
+							to: contractAddress, // Required except during contract publications.
+							from: account, // must match user's active address.
+							data: musomatic.methods
+								.createSong(_name, _artistName, window.web3.utils.toWei(_price, "Ether"), _imgHash, _songHash, _descriptionHash, _lyricsHash, _onSale, _links, _characteristics)
+								.encodeABI(), //make call to NFT smart contract
+						};
+
+						// Sign the transaction via Metamask
+						try {
+							const txHash = await window.ethereum.request({
+								method: "eth_sendTransaction",
+								params: [transactionParameters],
+							});
+							setLoading(false);
+							window.location.reload();
+							return {
+								success: true,
+								status: "Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" + txHash,
+							};
+						} catch (error) {
+							return {
+								success: false,
+								status: "Something went wrong: " + error.message,
+							};
+						}
+					});
+				});
+			});
+		});
+	}
+
+	async function purchaseSong(id, price, _artistAddress, _artistName) {
+		const _id = parseInt(id).toString();
+		// const _price = web3.utils.fromWei(price.toString(), 'Ether')
+
+		setLoading(true);
+
+		musomatic.methods
+			.purchaseSong(_id)
+			.send({ from: account, value: price })
+			.once("receipt", async (receipt) => {
+				setLoading(false);
+				window.location.reload();
+			})
+			.catch(function (error) {
+				if (error.code === 4001) {
+					window.location.reload();
+				}
+			});
+	}
+
+	async function toggleOnSale(id) {
+		const _id = parseInt(id).toString();
+
+		setLoading(true);
+		musomatic.methods
+			.toggleOnSale(_id)
+			.send({ from: account })
+			.once("receipt", (receipt) => {
+				setLoading(false);
+				window.location.reload();
+			})
+			.catch(function (error) {
+				if (error.code === 4001) {
+					window.location.reload();
+				} else {
+					// show error message
+				}
+			});
+	}
+
+	async function updatePrice(id, price) {
+		const _id = parseInt(id).toString();
+		const _price = web3.current.utils.toWei(price, "Ether");
+
+		setLoading(true);
+		musomatic.methods
+			.updatePrice(_id, _price)
+			.send({ from: account })
+			.once("receipt", (receipt) => {
+				setLoading(false);
+				window.location.reload();
+			})
+			.catch(function (error) {
+				if (error.code === 4001) {
+					window.location.reload();
+				}
+			});
+	}
+
 	return (
 		<HashRouter>
 			<ScrollToTop />
 			<Navbar />
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route exact path="/create" component={Create} />
+				<Route exact path="/library" render={() => (loading ? <Loading /> : <Library songNFTs={songNFTs} />)} />
+				<Route exact path="/trending" render={() => (loading ? <Loading /> : <Trending songNFTs={songNFTs} />)} />
+				<Route exact path="/dashboard" render={() => (loading ? <Loading /> : <Dashboard account={account} songNFTs={songNFTs} />)} />
+				<Route
+					exact
+					path="/create"
+					render={(props) =>
+						loading ? (
+							<Loading />
+						) : (
+							<Create
+								{...props}
+								createSong={createSong}
+								captureImage={captureImage}
+								songLyrics={songLyrics}
+								setSongLyrics={setSongLyrics}
+								captureLyrics={captureLyrics}
+								songDescription={songDescription}
+								setSongDescription={setSongDescription}
+								captureDescription={captureDescription}
+								captureSong={captureSong}
+								maticUSD={maticUSD}
+								maticINR={maticINR}
+							/>
+						)
+					}
+				/>
 				<Route path="/404" exact component={PageNotFound} />
-				<Route path="/library" exact component={Library} />
 				<Redirect to="/404" />
 			</Switch>
 			<Footer />
