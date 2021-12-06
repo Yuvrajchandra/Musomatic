5a28ec394d5c8a65db757a68532e5a93856c7324
Pushpit <pushpit07@gmail.com>
2021-12-06 05:42:56 +0530

    Updated Loading
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/components/Utils/Loading/Loading.js
+++ src/components/Utils/Loading/Loading.js
@@ -7,7 +7,7 @@ export default function LoadingSongInfo() {
 			<div className="loading_song_info_container">
 				<div className="loading_song_info_box">
 					<div className="loadingSpinner"></div>
-					<div className="classic-7"></div>
+					{/* <div className="classic-7"></div> */}
 					<div className="row p-0 m-0 mt-5">
 						<div className="col p-0 m-0 mt-5 wait_text">Please wait a moment. It takes some time to communicate with the blockchain.</div>
 					</div>
