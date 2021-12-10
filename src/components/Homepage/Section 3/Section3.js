0dbf463c2957e230f4b86f209b64723c422c235f
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-12-10 09:58:10 +0530

    Added Section 3 in HomePage

---
+++ src/components/Homepage/Section 3/Section3.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import "./section3.css";
+import Button from "../../Layout/Button/Button";
+import dashboardImage from "../../../assets/to-delete3.jpg";
+
+export default function Section2(){
+    return(
+        <Fragment>
+            <div className="section3-container">
+                <div className="section3-card">
+                    <div>
+                        <img className="dashboard-preview" src={dashboardImage} alt="dashboard preview" />
+                    </div>
+                    <div className="d-flex align-items-end flex-column">
+                        <div className="grey-sub-card d-flex flex-column ps-4">
+                            <div>
+                                <h3><strong>Creating NFT <br/>is so easy</strong></h3>
+                            </div>
+                            <div className="mt-4">
+                                <p>
+                                    Making the process of Creating and <br/>
+                                    Selling NFTs easy has always been our <br />
+                                    main goal. Our website UI places things <br />
+                                    that matter the most.
+                                </p>
+                            </div>
+                            <div className="d-flex mt-3">
+                                <div className="me-2">
+                                    <Button className="dashboard-button">Create NFT</Button>
+                                </div>
+                                <div className="ms-2">    
+                                    <Button className="dashboard-button">Explore NFTs</Button>
+                                </div>    
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
