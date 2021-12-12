b618d5e68605556b6437f28584b5b2f45fd5f7f7
Pushpit <pushpit07@gmail.com>
2021-12-12 13:46:12 +0530

    Updated social links in cover card
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/components/SongInfo/CoverCard/Socials/Socials.js
+++ src/components/SongInfo/CoverCard/Socials/Socials.js
@@ -15,25 +15,41 @@ export default function Socials(props) {
 					<a className="col-3" href={props.socials.spotify} target="_blank" rel="noopener noreferrer">
 						<img src={spotifyLogo} alt="spotify logo"></img>
 					</a>
-				) : null}
+				) : (
+					<a className="col-3" href="https://open.spotify.com/" target="_blank" rel="noopener noreferrer">
+						<img src={spotifyLogo} alt="spotify logo"></img>
+					</a>
+				)}
 
 				{props.socials.appleMusic ? (
 					<a className="col-3" href={props.socials.appleMusic} target="_blank" rel="noopener noreferrer">
 						<img src={appleLogo} alt="apple music logo"></img>
 					</a>
-				) : null}
+				) : (
+					<a className="col-3" href="https://music.apple.com/us/browse" target="_blank" rel="noopener noreferrer">
+						<img src={appleLogo} alt="apple music logo"></img>
+					</a>
+				)}
 
 				{props.socials.amazonMusic ? (
 					<a className="col-3 mt-1" href={props.socials.amazonMusic} target="_blank" rel="noopener noreferrer">
 						<img src={amazonLogo} alt="amazon music logo"></img>
 					</a>
-				) : null}
+				) : (
+					<a className="col-3 mt-1" href="https://music.amazon.in/" target="_blank" rel="noopener noreferrer">
+						<img src={amazonLogo} alt="amazon music logo"></img>
+					</a>
+				)}
 
 				{props.socials.youtubeMusic ? (
 					<a className="col-3" href={props.socials.youtubeMusic} target="_blank" rel="noopener noreferrer">
 						<img src={youtubeMusic} alt="amazon music logo"></img>
 					</a>
-				) : null}
+				) : (
+					<a className="col-3" href="https://music.youtube.com/" target="_blank" rel="noopener noreferrer">
+						<img src={youtubeMusic} alt="amazon music logo"></img>
+					</a>
+				)}
 			</div>
 		</Fragment>
 	);
