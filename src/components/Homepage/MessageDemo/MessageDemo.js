574ee487db913a050356ea2452563cbf801cb4c9
Pushpit <pushpit07@gmail.com>
2021-12-12 11:26:02 +0530

    Updated homepage styling
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/components/Homepage/MessageDemo/MessageDemo.js
+++ src/components/Homepage/MessageDemo/MessageDemo.js
@@ -1,47 +1,42 @@
-import React from 'react';
-import './MessageDemo.css';
-import ChatDemo1 from '../../../assets/Group333.svg'
-import ChatDemo2 from '../../../assets/Group334.svg'
-import Asset1 from '../../../assets/section4Asset1.png'
+import React from "react";
+import "./MessageDemo.css";
+import ChatDemo1 from "../../../assets/Group333.svg";
+import ChatDemo2 from "../../../assets/Group334.svg";
+import Asset1 from "../../../assets/section4Asset1.png";
 const MessageDemo = () => {
-    return (
-        <div className='message-demo-container'>
-            {/* <!-- header --> */}
-            <div className='message-demo-heading'>
-
-                <div>There's more to just</div>
-                <div>buy and sell NFTs</div>
-
-            </div>
-            <div >
-            <img className='asset1-img' src={Asset1} alt="assets-mobile"></img>
-            </div>
-
-            <div className='chat-sample-layout'>
-                <div className='message-card-1'>
-                    <div className='message-card-1-text'>
-                        <h1>Group Messages</h1>
-                        <p>Isn't it great that all your fans are in a same channel, giving feedbacks and thoughts.</p>
-                    </div>
-                    <div className='message-card-1-img'>
-                    <img src={ChatDemo1} alt="chat-demo-1"></img>
-                    </div>
-                </div>
-                <div className='message-card-2'>
-                    <div className='message-card-2-text'>
-                    <h1>Replies & Private Chat</h1>
-                        <p>Reply directly to a message for quick and short conversation</p>
-                    </div>
-                    <div className='message-card-2-img'>
-                    <img src={ChatDemo2} alt="chat-demo-2"></img>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-
-    );
-}
+	return (
+		<div className="message-demo-container">
+			<div className="message-demo-box">
+				{/* <!-- header --> */}
+				<div className="message-demo-heading">
+					<div>There's more to just</div>
+					<div>buy and sell NFTs</div>
+				</div>
+				<div>
+					<img className="asset1-img" src={Asset1} alt="assets-mobile"></img>
+				</div>
+				<div className="chat-sample-layout">
+					<div className="message-card-1 glass_effect glass_effect_border">
+						<div className="message-card-1-text">
+							<h1>Group Messages</h1>
+							<p>Isn't it great that all your fans are in a same channel, giving feedbacks and thoughts.</p>
+						</div>
+						<div className="message-card-1-img">
+							<img src={ChatDemo1} alt="chat-demo-1"></img>
+						</div>
+					</div>
+					<div className="message-card-2 glass_effect glass_effect_border">
+						<div className="message-card-2-text">
+							<h1>Replies & Private Chat</h1>
+							<p>Reply directly to a message for quick and short conversation</p>
+						</div>
+						<div className="message-card-2-img">
+							<img src={ChatDemo2} alt="chat-demo-2"></img>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 export default MessageDemo;
--- src/components/Homepage/Section 3/Section3.js
+++ src/components/Homepage/Section 3/Section3.js
@@ -1,39 +1,43 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import "./section3.css";
 import Button from "../../Layout/Button/Button";
 
-export default function Section3(){
-    return(
-        <Fragment>
-            <div className="section3-container row m-0 justify-content-center">
-                <div className="section3-card d-flex">
-                    <div className="dashboard-preview">
-                    </div>
-                    <div className="col d-flex flex-column align-items-end m-0 p-0">
-                        <div className="d-flex flex-column grey-sub-card">
-                            <div>
-                                <h3><strong>Creating NFT <br/>is so easy</strong></h3>
-                            </div>
-                            <div className="mt-4">
-                                <p>
-                                    Making the process of Creating and
-                                    Selling NFTs easy has always been our 
-                                    main goal. Our website UI places things 
-                                    that matter the most.
-                                </p>
-                            </div>
-                            <div className="d-flex mt-4">
-                                <div className="me-2 flex-fill">
-                                    <Button>Create</Button>
-                                </div>
-                                <div className="ms-2 flex-fill">    
-                                    <Button>Library</Button>
-                                </div>    
-                            </div> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Fragment>
-    );
+export default function Section3() {
+	return (
+		<Fragment>
+			<div className="section3-container row m-0 justify-content-center">
+				<div className="section3-card d-flex">
+					<div className="dashboard-preview"></div>
+					<div className="col d-flex flex-column align-items-end m-0 p-0">
+						<div className="d-flex flex-column grey-sub-card">
+							<div>
+								<h3>
+									<strong>
+										Creating NFT <br />
+										is so easy
+									</strong>
+								</h3>
+							</div>
+							<div className="mt-4">
+								<p>Making the process of Creating and Selling NFTs easy has always been our main goal. Our website UI places things that matter the most.</p>
+							</div>
+							<div className="d-flex mt-4">
+								<div className="me-2 flex-fill">
+									<Link to={"/create"}>
+										<Button>Create</Button>
+									</Link>
+								</div>
+								<div className="ms-2 flex-fill">
+									<Link to={"/library"}>
+										<Button>Library</Button>
+									</Link>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 }
