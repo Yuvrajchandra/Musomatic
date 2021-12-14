60a9a38dc3c92e715ff9759187cf792eaaf0fe30
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-12-14 22:34:38 +0530

    Made Hero section responsive

--- src/components/Homepage/Hero/Hero.js
+++ src/components/Homepage/Hero/Hero.js
@@ -8,8 +8,14 @@ import hero2 from "../../../assets/hero-img-2.png";
 import hero3 from "../../../assets/hero-img-3.png";
 import hero4 from "../../../assets/hero-img-4.png";
 
+import hero1_m from "../../../assets/hero-img-m-1.png";
+import hero2_m from "../../../assets/hero-img-m-2.png";
+import hero3_m from "../../../assets/hero-img-m-3.png";
+import hero4_m from "../../../assets/hero-img-m-4.png";
+
 export default function Hero() {
 	const fadeImages = [hero1, hero2, hero3, hero4];
+	const fadeImages_m = [hero1_m, hero2_m, hero3_m, hero4_m];
 
 	const properties = {
 		// duration: 5000,
@@ -26,21 +32,25 @@ export default function Hero() {
 				<div className="each-fade">
 					<div className="image-container">
 						<img className="hero-container-new" src={fadeImages[0]} alt="" />
+						<img className="hero-container-new-m" src={fadeImages_m[0]} alt="" />
 					</div>
 				</div>
 				<div className="each-fade">
 					<div className="image-container">
 						<img className="hero-container-new" src={fadeImages[1]} alt="" />
+						<img className="hero-container-new-m" src={fadeImages_m[1]} alt="" />
 					</div>
 				</div>
 				<div className="each-fade">
 					<div className="image-container">
 						<img className="hero-container-new" src={fadeImages[2]} alt="" />
+						<img className="hero-container-new-m" src={fadeImages_m[2]} alt="" />
 					</div>
 				</div>
 				<div className="each-fade">
 					<div className="image-container">
 						<img className="hero-container-new" src={fadeImages[3]} alt="" />
+						<img className="hero-container-new-m" src={fadeImages_m[3]} alt="" />
 					</div>
 				</div>
 			</Fade>
